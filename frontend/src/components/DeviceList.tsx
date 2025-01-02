6f4f2496dec0c93982f807e2c72aa235d8d60aba
Simen Kvalø <han.simen.kvalo@gmail.com>
2025-01-02 18:47:16 +0100

    Performance fixes.

--- frontend/src/components/DeviceList.tsx
+++ frontend/src/components/DeviceList.tsx
@@ -2,11 +2,10 @@ import React, { useEffect, useMemo, useState } from "react";
 import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
 import axios from "axios";
-import {Map } from "./Map";
+import { MapBox } from "./Map";
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 export interface device {
-  id: number;
   name: string;
   location: string;
   status: string;
@@ -21,14 +20,17 @@ interface apiResponse {
 }
 
 export default function DeviceList() {
-
   // Row Data: The data to be displayed.
-  const [rowData, setRowData] = useState<any[]>([]);
+  const [rowData, setRowData] = useState<device[]>([]);
   // Loading state to ensure asynchronosity.
   const [loading, setLoading] = useState(true);
 
   // Column Definitions: Defines the columns to be displayed.
-  const [colDefs] = useState<any[]>([{ field: "name" }, { field: "status" }, { field: "location"}]);
+  const [colDefs] = useState<any[]>([
+    { field: "name" },
+    { field: "status" },
+    { field: "location" },
+  ]);
 
   // Due to data fetching being asynchronous we have to use useEffect. This ensures
   // that the data gets fetched before the component mounts.
@@ -36,12 +38,17 @@ export default function DeviceList() {
     async function fetchData() {
       try {
         const response = await axios.get("http://localhost:3000");
+
         const devices: apiResponse[] = response.data;
 
         // Map apiResponses to objects of type Device.
         const rowData = devices.map((device) => {
-            const deviceLocation = "" + device.latitude + ", " + device.longitude;
-            return { name: device.name, status: device.status, location: deviceLocation };
+          const deviceLocation = "" + device.latitude + ", " + device.longitude;
+          return {
+            name: device.name,
+            status: device.status,
+            location: deviceLocation,
+          };
         });
 
         setRowData(rowData);
@@ -59,8 +66,8 @@ export default function DeviceList() {
 
   const handleRowClick = (event: any) => {
     const selectedDevice = event.data;
-    setSelectedDevice(selectedDevice)
-  }
+    setSelectedDevice(selectedDevice);
+  };
 
   // Functionality for sorting, connected to the checkbox. ag-grid itself supports
   // binary sorting, but it's locked behind a pay-wall, so i implemented it myself.
@@ -68,28 +75,33 @@ export default function DeviceList() {
 
   const handleCheckboxChange = () => {
     setShowInactive(!showInactive);
-  }
+  };
 
+  // Use memo for caching.
   const filteredRowData = useMemo(() => {
-    if (showInactive) {
-      return rowData;
-    }
-    return rowData.filter((device) => device.status === "active")
+    return showInactive
+      ? rowData
+      : rowData.filter((device) => device.status === "active");
   }, [showInactive, rowData]);
 
   if (loading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   return (
     <>
       <div className="flex flex-col h-screen w-screen">
-        {/* Show Inactive text at the top */}
+        {/* Show Inactive text + checkbox at the top */}
         <div className="text-left pl-8 pt-4 flex items-center">
           <p>Show inactive: </p>
-          <input defaultChecked={showInactive} onChange={handleCheckboxChange} className="ml-4" type="checkbox"/>
+          <input
+            defaultChecked={showInactive}
+            onChange={handleCheckboxChange}
+            className="ml-4"
+            type="checkbox"
+          />
         </div>
-  
+
         <div className="flex flex-1">
           {/* Left side: Ag Grid */}
           <div className="flex-1 p-4">
@@ -99,10 +111,13 @@ export default function DeviceList() {
               onRowClicked={handleRowClick}
             />
           </div>
-  
           {/* Right side: Map */}
           <div className="flex-1 p-4">
-            <Map devices={rowData} selectedDevice={selectedDevice} showInactive={showInactive} />
+            <MapBox
+              devices={rowData}
+              selectedDevice={selectedDevice}
+              showInactive={showInactive}
+            />
           </div>
         </div>
       </div>
--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,26 +1,31 @@
-import React, { useRef, useEffect, useMemo } from "react";
+import React, { useRef, useEffect } from "react";
 import mapboxgl, { Marker } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-interface MapProps {
-  devices: {name: string, status: string, location: string}[];
+interface MapBoxProps {
+  devices: { name: string; status: string; location: string }[];
   selectedDevice: { name: string; status: string; location: string } | null;
   showInactive: boolean;
 }
 
-const markers: Marker[] = [] 
-
-export const Map: React.FC<MapProps> = ({ devices, selectedDevice, showInactive }) => {
-
+export const MapBox: React.FC<MapBoxProps> = ({
+  devices,
+  selectedDevice,
+  showInactive,
+}) => {
   const mapRef = useRef(null);
   const mapContainerRef = useRef(null);
 
-  const markersRef = useRef<Marker[]>([]);
+  // Keep track of markers with device for easy removal.
+  const markersRef = useRef<Map<string, Marker>>(new Map());
 
+  // Map init.
   useEffect(() => {
-    mapboxgl.accessToken = "pk.eyJ1IjoiaHNqb2hhbnNlbiIsImEiOiJjbTVlOWQ1cDAyNnR4MmxyNzJtZmhvMmVmIn0.aRUwNHNNmYO7e0TrCs7Ksg";
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiaHNqb2hhbnNlbiIsImEiOiJjbTVlOWQ1cDAyNnR4MmxyNzJtZmhvMmVmIn0.aRUwNHNNmYO7e0TrCs7Ksg";
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
+      fadeDuration: 0,
     });
 
     return () => {
@@ -29,40 +34,52 @@ export const Map: React.FC<MapProps> = ({ devices, selectedDevice, showInactive 
   }, []);
 
   useEffect(() => {
-    markersRef.current.forEach((marker) => {
-      marker.remove();
-    })
+    // Remove all inactive markers if checkbox unchecked.
+    if (!showInactive) {
+      devices
+        .filter((device) => device.status === "inactive")
+        .forEach((device) => {
+          const marker = markersRef.current.get(device.name);
+          if (marker) {
+            marker.remove();
+            markersRef.current.delete(device.name);
+          }
+        });
+    }
 
-    markersRef.current = []
-
-    const filteredDevices = showInactive ? devices : devices.filter((device) => device.status === "active")
-
-    filteredDevices.forEach((device) => {
+    // Draw all active markers, or all markers if showInactive is true.
+    devices.forEach((device) => {
       const latitude = device.location.split(",")[0];
       const longitude = device.location.split(",")[1];
 
-      const marker = new mapboxgl.Marker().setLngLat([parseFloat(longitude), parseFloat(latitude)]).addTo(mapRef.current);
-      markersRef.current.push(marker);
+      if (showInactive || device.status == "active") {
+        const marker = new mapboxgl.Marker()
+          .setLngLat([parseFloat(longitude), parseFloat(latitude)])
+          .addTo(mapRef.current);
+
+        markersRef.current.set(device.name, marker);
+      }
     });
-    console.log(markersRef.current.length)
-  }, [showInactive])
+  }, [showInactive]);
 
   // Functionality for flying to a clicked device.
   useEffect(() => {
     if (selectedDevice) {
-      console.log("Clicked: ", selectedDevice)
-      const latitude = selectedDevice.location.split(",")[0]
-      const longitude = selectedDevice.location.split(",")[1]
+      console.log("Clicked: ", selectedDevice);
+      const latitude = selectedDevice.location.split(",")[0];
+      const longitude = selectedDevice.location.split(",")[1];
 
-      console.log("Flying to: ", longitude, latitude)
+      console.log("Flying to: ", longitude, latitude);
       mapRef.current.flyTo({
         center: [longitude, latitude],
         essential: true,
         speed: 1,
-        zoom: 12
-      })
+        zoom: 12,
+      });
     }
-  },  [selectedDevice]) // Called whenever selectedDevice changes, aka when the user clicks an entry in the list.
+  }, [selectedDevice]); // Called whenever selectedDevice changes, aka when the user clicks an entry in the list.
 
-  return <div className="h-full w-full" id="map-container" ref={mapContainerRef} />;
-}
+  return (
+    <div className="h-full w-full" id="map-container" ref={mapContainerRef} />
+  );
+};
